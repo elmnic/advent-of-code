8c6b19c31c85ad38d7084da26798ab2385e8802c
Niclas Elmäng <niclas.elmang@pagero.com>
2021-12-12 02:00:27 +0100

    Added Day 7 skeleton

---
+++ scala/AoC-2021/src/main/scala/days/Day7.scala
@@ -0,0 +1,12 @@
+package days
+
+class Day7 {
+  def solve(input: Seq[String]): (Long, Long) = {
+
+    val resultPart1 = 0
+
+    val resultPart2 = 0
+
+    (resultPart1, resultPart2)
+  }
+}
---
+++ scala/AoC-2021/src/test/scala/days/Day7Spec.scala
@@ -0,0 +1,23 @@
+package days
+
+import org.scalatest.DoNotDiscover
+import util.TestInputHelper
+
+@DoNotDiscover
+class Day7Spec extends BaseTest {
+
+  "Day7" should "return the sample output given the sample input" in {
+    val testInput = TestInputHelper.getTestInput("7") match {
+      case Left(msg)    => fail(msg)
+      case Right(input) => input
+    }
+    val (part1, part2) = new Day7().solve(testInput)
+
+    withClue("Part1: ") {
+      part1 shouldBe 37
+    }
+    withClue("Part2: ") {
+      part2 shouldBe 5
+    }
+  }
+}
