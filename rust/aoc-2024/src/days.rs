a81eae773e20b7fa744ac30cdde1c946f404c951
Niclas Elmäng <niclas.elmang@outlook.com>
2024-12-15 22:53:48 +0100

    Day 2 (#3)

--- rust/aoc-2024/src/days.rs
+++ rust/aoc-2024/src/days.rs
@@ -1,8 +1,14 @@
 use itertools::Itertools;
 use regex::Regex;
+use std::arch::x86_64::_xgetbv;
 use std::iter::zip;
+use std::ops::Neg;
 
 pub fn day1(input: Vec<&str>) -> (i64, i64) {
+    fn parse_capture(capture: &str) -> i64 {
+        capture.parse().unwrap()
+    }
+
     let re = Regex::new(r"(\d+)\s{3}(\d+)").unwrap();
 
     let parsed_input: Vec<_> = input
@@ -48,6 +54,74 @@ pub fn day1(input: Vec<&str>) -> (i64, i64) {
     (distances.iter().sum(), part2.iter().sum())
 }
 
-fn parse_capture(capture: &str) -> i64 {
-    capture.parse().unwrap()
+pub fn day2(input: Vec<&str>) -> (i64, i64) {
+    fn check_safe(report: &Vec<i64>) -> bool {
+        let is_ascending = report.is_sorted_by(|l, r| l < r);
+        let is_descending = report.is_sorted_by(|l, r| l > r);
+        let is_close_enough = report.is_sorted_by(|left, right| {
+            let diff = (left.clone() - right.clone()).abs();
+            diff >= 1 && diff <= 3
+        });
+        let is_safe = (is_ascending || is_descending) && is_close_enough;
+        is_safe
+    }
+
+    fn recheck_unsafe(report: &Vec<i64>) -> bool {
+        // TODO: return early?
+        let mut is_safe_after_level_removed = false;
+        for level in 0..report.len() {
+            let mut report_with_level_removed = report.clone();
+            report_with_level_removed.remove(level);
+            let is_modified_safe = check_safe(&report_with_level_removed);
+            if is_modified_safe {
+                is_safe_after_level_removed = true;
+            }
+        }
+        is_safe_after_level_removed
+    }
+
+    fn check_safe_2(report: &Vec<i64>) -> bool {
+        let is_ascending = report.is_sorted_by(|l, r| l < r);
+        let is_descending = report.is_sorted_by(|l, r| l > r);
+
+        // Check for any levels that break directionality if vec is not sorted in either direction
+        if !is_ascending && !is_descending {
+            recheck_unsafe(report)
+        } else {
+            let is_close_enough = report.is_sorted_by(|left, right| {
+                let diff = (left.clone() - right.clone()).abs();
+                let is_safe = diff >= 1 && diff <= 3;
+                // if !is_safe {
+                //     println!("\tleft: {:?}, right: {:?}, diff: {:?}, correct_dir: {:?}, is_safe: {:?}", left, right, diff_abs, correct_direction, is_safe);
+                // }
+                is_safe
+            });
+
+            // Direction has been checked, either the levels are close enough already or there might be one level that can be removed to make it so
+            is_close_enough || recheck_unsafe(report)
+        }
+    }
+
+    let parsed_input: Vec<_> = input
+        .iter()
+        .map(|line| {
+            line.split(" ")
+                .map(|x| x.parse::<i64>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let result1 = parsed_input
+        .iter()
+        .map(|line| check_safe(line))
+        .filter(|x| *x)
+        .count();
+
+    let result2 = parsed_input
+        .iter()
+        .map(|line| check_safe_2(line))
+        .filter(|x| *x)
+        .count();
+
+    (result1 as i64, result2 as i64)
 }
